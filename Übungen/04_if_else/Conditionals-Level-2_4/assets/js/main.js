90ccfae705688480ae2595c60364f59dcbdefb03
Frederick Pawelzik <frederickpawelzik@gmail.com>
2024-01-31 19:44:02 +0100

    Kommentare zu Conditionals Level 2_4 hinzugefügt

--- Übungen/04_if_else/Conditionals-Level-2_4/assets/js/main.js
+++ Übungen/04_if_else/Conditionals-Level-2_4/assets/js/main.js
@@ -1,3 +1,8 @@
+//* Aufgabenstellung:
+// Schreibe eine Funktion mit zwei Parametern (Zahlen).
+// Die Funktion soll die Summe dieser Zahlen berechnen.
+// Wenn beide Parameter gleich sind, soll sie (Summe 5) ausgeben.
+
 function conditionals(num1, num2) {
   const ergebnis = num1 + num2;
 
