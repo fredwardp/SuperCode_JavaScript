312a170b7704a32d129c823ad5ff752ec10bbe78
Frederick Pawelzik <frederickpawelzik@gmail.com>
2024-01-31 19:31:34 +0100

    Feintuning der Aufgaben

--- Übungen/04_if_else/Conditionals-Level-2_2/assets/js/main.js
+++ Übungen/04_if_else/Conditionals-Level-2_2/assets/js/main.js
@@ -1,22 +1,22 @@
 function airQuality() {
   const range = document.querySelector(".range").value;
   const background = document.querySelector("body");
-  const p = document.querySelector("p");
+  const label = document.querySelector("label");
   const firstH2 = document.querySelector(".concern");
   const secondH2 = document.querySelector(".effect");
 
   if (range < 55) {
-    p.innerHTML += range;
+    label.innerHTML = "Luftqualität: " + range;
     firstH2.innerHTML = "Level of health concern: good";
     secondH2.innerHTML = "Level of health effect: Little or no risk";
     background.style.backgroundColor = "#008200";
   } else if (range < 94) {
-    p.innerHTML += range;
+    label.innerHTML = "Luftqualität: " + range;
     firstH2.innerHTML = "Level of health concern: Moderate";
     secondH2.innerHTML = "Level of health effect: Acceptable quality";
     background.style.backgroundColor = "#FFF300";
   } else {
-    p.innerHTML += range;
+    label.innerHTML = "Luftqualität: " + range;
     firstH2.innerHTML =
       "Level of health concern: Unhealthy for sensitive groups";
     secondH2.innerHTML =
