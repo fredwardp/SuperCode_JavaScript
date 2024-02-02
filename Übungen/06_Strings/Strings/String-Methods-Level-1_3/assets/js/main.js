a43d5c7eecdb3e0e15cf2ae9c78af88e4a032a3c
Frederick Pawelzik <frederickpawelzik@gmail.com>
2024-02-02 13:36:32 +0100

    Aufgabe String Methods Level 1_3

---
+++ Übungen/06_Strings/Strings/String-Methods-Level-1_3/assets/js/main.js
@@ -0,0 +1,11 @@
+let txt2 =
+  "Blue, green, grey, white, or black; smooth, ruffled, or mountainous; that ocean is not silent.";
+
+console.log(txt2.indexOf(";"));
+console.log(txt2.search(";"));
+
+console.log(txt2.indexOf("green"));
+console.log(txt2.search("green"));
+
+console.log(txt2.indexOf("blue"));
+console.log(txt2.search("blue"));
