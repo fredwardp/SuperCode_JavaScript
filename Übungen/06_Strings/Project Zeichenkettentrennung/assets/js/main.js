2a6ef345c57ffb46986cdefdb51b6b24d8e831ab
Frederick Pawelzik <frederickpawelzik@gmail.com>
2024-02-02 16:53:18 +0100

    Console.logs entfernt

--- Übungen/06_Strings/Project Zeichenkettentrennung/assets/js/main.js
+++ Übungen/06_Strings/Project Zeichenkettentrennung/assets/js/main.js
@@ -9,16 +9,13 @@ function hierTrennen() {
   const hintererTeil = document.querySelector(".hinterer_teil");
 
   const trennWert = zeichenkette.indexOf(trennenBei);
-  //   console.log(trennWert);
+
   const trennWertUndEins = trennWert + 1;
-  //   console.log(trennWertUndEins);
 
   if (davor == "1") {
-    console.log("davor ist true");
     vordererTeil.textContent = zeichenkette.slice(0, trennWert);
     hintererTeil.textContent = zeichenkette.slice(trennWert, -1);
   } else {
-    console.log("danach ist true");
     vordererTeil.textContent = zeichenkette.slice(0, trennWertUndEins);
     hintererTeil.textContent = zeichenkette.slice(trennWertUndEins, -1);
   }
