6b5febd7e89d5a2eae4723a13c543784f7bcdfef
Frederick Pawelzik <frederickpawelzik@gmail.com>
2024-02-05 16:11:15 +0100

    Fuctions Vertiefung Aufgaben 1 un 2

---
+++ Übungen/07_stringmethods_return/Fuctions_Vertiefung/Functions-Vertiefung-Level-1_7/assets/js/main.js
@@ -0,0 +1,8 @@
+const newFunction = (param1) => {
+  ergebnis = param1 * 2;
+  return ergebnis;
+};
+
+ausgabe = newFunction(5);
+
+console.log(ausgabe);
--- Übungen/07_stringmethods_return/String_methods/String-Methods-Level-2_3/assets/js/main.js
+++ Übungen/07_stringmethods_return/String_methods/String-Methods-Level-2_3/assets/js/main.js
@@ -4,4 +4,7 @@ const marker = () => {
 
   articleText = articleText.replaceAll(input, `<span> ${input} </span>`);
   document.querySelector("article").innerHTML = articleText;
+
+  const styling = document.querySelector("span");
+  styling.classList.add("highlight");
 };
