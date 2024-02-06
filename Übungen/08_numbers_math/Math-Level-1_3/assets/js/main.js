827936125a17ff200ef3e561887743bf57b92bb4
Frederick Pawelzik <frederickpawelzik@gmail.com>
2024-02-06 13:39:51 +0100

    Math Level Aufgabe 1_2 bis 1_4

---
+++ Übungen/08_numbers_math/Math-Level-1_3/assets/js/main.js
@@ -0,0 +1,8 @@
+let randomNum = Math.random();
+console.log(randomNum);
+
+let randomNum1_10 = Math.random() * 10;
+console.log(Math.ceil(randomNum1_10));
+
+let randomNum1_100 = Math.random() * 100;
+console.log(Math.ceil(randomNum1_100));
