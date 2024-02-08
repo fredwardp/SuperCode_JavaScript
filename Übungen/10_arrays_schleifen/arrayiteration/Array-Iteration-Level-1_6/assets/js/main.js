54e6f43d72c96e66ed5449cac16b37f0deb573be
Frederick Pawelzik <frederickpawelzik@gmail.com>
2024-02-08 16:49:18 +0100

    Array Iteration Level 1_6

--- Übungen/10_arrays_schleifen/arrayiteration/Array-Iteration-Level-1_6/assets/js/main.js
+++ Übungen/10_arrays_schleifen/arrayiteration/Array-Iteration-Level-1_6/assets/js/main.js
@@ -6,3 +6,13 @@ let album = [
   "DOGATBEACH.jpg",
 ];
 
+const noDots = album.map((ending) => {
+  let dot = ending.indexOf(".");
+  if (dot >= 0) {
+    console.log(ending.slice(0, dot).toLowerCase());
+  } else {
+    console.log("invalid");
+  }
+
+  return dot;
+});
